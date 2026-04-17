ba529cdbd6ecbc0851468e4f930ea03cfc048b43
jimenezeljay <143433352+jimenezeljay@users.noreply.github.com>
2026-04-17 12:03:18 -0400

    channels

--- main.go
+++ main.go
@@ -2,21 +2,27 @@ package main
 
 import "fmt"
 
+func double(number int, ch chan int) {
+	result := number * 2
+	ch <- result // Send result back through the channel
+}
+
 func main() {
+	numbers := []int{1, 2, 3, 4, 5}
 
-	//creating a slice
-	numbers := []int{10, 20, 30, 40, 50}
+	// Create a channel that carries ints
+	ch := make(chan int)
 
-	//accessing by index
-	fmt.Println("First:", numbers[0])
-	fmt.Println("Last:", numbers[4])
-
-	//looping over a slice with range
-	for index, value := range numbers {
-		fmt.Println("Index:", index, "Value:", value)
+	// Launch one goroutine per number
+	for _, num := range numbers {
+		go double(num, ch)
 	}
 
-	//adding to a slice with append
-	numbers = append(numbers, 60)
-	fmt.Println("After append:", numbers)
+	// Collect ALL results back from the channel
+	for i := 0; i < len(numbers); i++ {
+		result := <-ch
+		fmt.Println("Got result:", result)
+	}
+
+	fmt.Println("All done!")
 }
